44fcf0995cfb8026df5d3e6e45e57eac1a6a75e0
Chrispo <christiancastillo0350@gmail.com>
2025-03-07 01:20:28 -0800

    Funcion de agregar amigo y obtener elemento añadidas

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+
+function elementoPorID(ID){
+    return document.getElementById(ID);
+}
+
+function agregarAmigo(){
+    let amigoInput = elementoPorID('amigo')
+    let amigo = amigoInput.value.trim();
+    if(amigo == ""){
+        alert("No se admiten espacios vacios");
+    }else{
+        //console.log(`Añadiendo a: ${amigo}`);
+        nombresAmigos.push(amigo);
+    }
+    //console.log('Borrando input');
+    amigoInput.value = "";
+}
